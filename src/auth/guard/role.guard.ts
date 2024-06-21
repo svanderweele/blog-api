38ba5d7cf4938ea38168ee9f4eceaa1d653cf025
Simon van der Weele <vanderweelesimon@gmail.com>
2024-06-21 12:14:36 +0200

    feat: added authorization (#5)

---
+++ src/auth/guard/role.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../enums/role.enum';
+import { ROLES_KEY } from '../decorator/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+
+    // if user is an admin he can access anything
+    if (user.roles?.includes(Role.Admin)) {
+      return true;
+    }
+
+    return requiredRoles.some((role) => user.roles?.includes(role));
+  }
+}
